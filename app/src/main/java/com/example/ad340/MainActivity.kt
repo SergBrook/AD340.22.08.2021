a26622123860b5b278f6f9b57877a8f8785caee5
bss65500 <masid2000@ukr.net>
2021-09-08 19:48:05 +0300

    Diffutil

--- app/src/main/java/com/example/ad340/MainActivity.kt
+++ app/src/main/java/com/example/ad340/MainActivity.kt
@@ -5,8 +5,16 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private val forecastRepository = ForecastRepository()
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,11 +26,22 @@ class MainActivity : AppCompatActivity() {
             if (zipcode.length !=5) {
                 Toast.makeText(this, "eror",Toast.LENGTH_SHORT).show()
             } else {
-                Toast.makeText(this, zipcode, Toast.LENGTH_SHORT).show()
+                forecastRepository.loadForecast(zipcode)
+
             }
         }
+          val forecastList : RecyclerView = findViewById(R.id.forecastList)
+          forecastList.layoutManager = LinearLayoutManager(this)
+            val dailyForecastAdapter = DailyForecastAdapter()
+           forecastList.adapter = dailyForecastAdapter
 
 
+
+          val weeklyForecastObserver = Observer<List<DailyForecast>> {forecastItems ->
+              //update our list adapter
+           dailyForecastAdapter.submitList(forecastItems)
+          }
+            forecastRepository.weeklyForecast.observe(this, weeklyForecastObserver)
     }
 
 
